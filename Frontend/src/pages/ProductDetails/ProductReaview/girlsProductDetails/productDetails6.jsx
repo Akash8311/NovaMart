2705eef686d74051c080da337d26e07a0a4bc7c9
Akash Maity <am6030920@gmail.com>
2026-05-08 13:16:19 +0530

    add product6

---
+++ Frontend/src/pages/ProductDetails/ProductReaview/girlsProductDetails/productDetails6.jsx
@@ -0,0 +1,42 @@
+import React from "react";
+import Breadcrumbs from "@mui/material/Breadcrumbs";
+import Link from "@mui/material/Link";
+import HomeIcon from "@mui/icons-material/Home";
+import WhatshotIcon from "@mui/icons-material/Whatshot";
+import { Link as RouterLink } from "react-router-dom";
+
+import ProductZoom5 from "../../../../components/ProductZoom/GirlsProductZoom/ProductZoom5";
+import { IoShirt } from "react-icons/io5";
+import { CiTrophy } from "react-icons/ci";
+import { RiSecurePaymentLine } from "react-icons/ri";
+import { useState } from "react";
+import Qtybox from "../../../../components/QtyBox/Qtybox";
+import "../../Product.css";
+import GirlsProducReview4 from "../girlsProductReview/girlsProducReview4";
+import { Swiper, SwiperSlide } from "swiper/react";
+
+import "swiper/css";
+import "swiper/css/pagination";
+
+import { Pagination } from "swiper/modules";
+import ProductItems from "../../../../components/ProductItems/ProductItems";
+import ProductItems2 from "../../../../components/ProductItems/ProductItems2";
+import ProductItems3 from "../../../../components/ProductItems/ProductItems3";
+import ProductItems4 from "../../../../components/ProductItems/ProductItems4";
+import ProductItems6 from "../../../../components/ProductItems/ProductItems6";
+import ProductItems7 from "../../../../components/ProductItems/ProductItems7";
+import ProductItems8 from "../../../../components/ProductItems/ProductItems8";
+import ProductItems9 from "../../../../components/ProductItems/ProductItems9";
+import ProductItems10 from "../../../../components/ProductItems/ProductItems10";
+import ProductItems11 from "../../../../components/ProductItems/ProductItems11";
+import ProductItems12 from "../../../../components/ProductItems/ProductItems12";
+import { useContext } from "react";
+import { MyContext } from "../../../../App";
+
+const productDetails6 = () => {
+  return (
+    <div>productDetails6</div>
+  )
+}
+
+export default productDetails6
