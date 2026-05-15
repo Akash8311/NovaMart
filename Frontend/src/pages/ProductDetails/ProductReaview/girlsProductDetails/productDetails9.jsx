38e8eea87c9b36b4a874774dfb3a2869b93cd46e
Akash Maity <am6030920@gmail.com>
2026-05-15 22:45:06 +0530

    add product

--- Frontend/src/pages/ProductDetails/ProductReaview/girlsProductDetails/productDetails9.jsx
+++ Frontend/src/pages/ProductDetails/ProductReaview/girlsProductDetails/productDetails9.jsx
@@ -1,4 +1,34 @@
-import React from 'react'
+import React from "react";
+import Breadcrumbs from "@mui/material/Breadcrumbs";
+import Link from "@mui/material/Link";
+import HomeIcon from "@mui/icons-material/Home";
+import WhatshotIcon from "@mui/icons-material/Whatshot";
+import { Link as RouterLink } from "react-router-dom";
+import ProductZoom8 from "../../../../components/ProductZoom/GirlsProductZoom/ProductZoom8";
+import { IoShirt } from "react-icons/io5";
+import { CiTrophy } from "react-icons/ci";
+import { RiSecurePaymentLine } from "react-icons/ri";
+import { useState } from "react";
+import Qtybox from "../../../../components/QtyBox/Qtybox";
+import "../../Product.css";
+import GirlsProducReview4 from "../girlsProductReview/girlsProducReview4";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/pagination";
+import { Pagination } from "swiper/modules";
+import ProductItems from "../../../../components/ProductItems/ProductItems";
+import ProductItems2 from "../../../../components/ProductItems/ProductItems2";
+import ProductItems3 from "../../../../components/ProductItems/ProductItems3";
+import ProductItems4 from "../../../../components/ProductItems/ProductItems4";
+import ProductItems6 from "../../../../components/ProductItems/ProductItems6";
+import ProductItems5 from "../../../../components/ProductItems/ProductItems5";
+import ProductItems7 from "../../../../components/ProductItems/ProductItems7";
+import ProductItems9 from "../../../../components/ProductItems/ProductItems9";
+import ProductItems10 from "../../../../components/ProductItems/ProductItems10";
+import ProductItems11 from "../../../../components/ProductItems/ProductItems11";
+import ProductItems12 from "../../../../components/ProductItems/ProductItems12";
+import { useContext } from "react";
+import { MyContext } from "../../../../App";
 
 const productDetails9 = () => {
   return (
