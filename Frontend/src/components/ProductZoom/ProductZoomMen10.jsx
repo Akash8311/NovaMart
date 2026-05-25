44526143526585f24a7c1e34cd55b5ac4750c230
Akash Maity <am6030920@gmail.com>
2026-05-25 18:37:50 +0530

    BOYS 10

--- Frontend/src/components/ProductZoom/ProductZoomMen10.jsx
+++ Frontend/src/components/ProductZoom/ProductZoomMen10.jsx
@@ -1,11 +1,11 @@
 import React, { useRef, useState } from "react";
 import "./ProductZoom.css";
 const images = [
-  "https://m.media-amazon.com/images/I/91WG7Ztkd9L._SY741_.jpg",
-  "https://m.media-amazon.com/images/I/815VLA5aj1L._SY741_.jpg",
-  "https://m.media-amazon.com/images/I/81FaG-M4PWL._SY741_.jpg",
-  "https://m.media-amazon.com/images/I/81SgItg3HEL._SY741_.jpg",
-  "https://m.media-amazon.com/images/I/81TZjMlGWcL._SY741_.jpg",
+  "https://m.media-amazon.com/images/I/61e0-selrtL._SY741_.jpg",
+  "https://m.media-amazon.com/images/I/71s30ZEcV9L._SX679_.jpg",
+  "https://m.media-amazon.com/images/I/61Pcr9GuziL._SY741_.jpg",
+  "https://m.media-amazon.com/images/I/61e0-selrtL._SY741_.jpg",
+  "https://m.media-amazon.com/images/I/61Ez0dKIvTL._SX569_.jpg",
 
 ];
 const ProductZoomMen10 = () =>{
