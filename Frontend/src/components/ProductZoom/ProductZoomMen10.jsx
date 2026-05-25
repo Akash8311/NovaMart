2a553188e4b14587ff56163bb7e3e0695dcba036
Akash Maity <am6030920@gmail.com>
2026-05-25 13:11:07 +0530

    add file

--- Frontend/src/components/ProductZoom/ProductZoomMen10.jsx
+++ Frontend/src/components/ProductZoom/ProductZoomMen10.jsx
@@ -1,5 +1,13 @@
-import React from 'react'
+import React, { useRef, useState } from "react";
+import "./ProductZoom.css";
+const images = [
+  "https://m.media-amazon.com/images/I/91WG7Ztkd9L._SY741_.jpg",
+  "https://m.media-amazon.com/images/I/815VLA5aj1L._SY741_.jpg",
+  "https://m.media-amazon.com/images/I/81FaG-M4PWL._SY741_.jpg",
+  "https://m.media-amazon.com/images/I/81SgItg3HEL._SY741_.jpg",
+  "https://m.media-amazon.com/images/I/81TZjMlGWcL._SY741_.jpg",
 
+];
 const ProductZoomMen10 = () => {
   return (
     <div>ProductZoomMen10</div>
