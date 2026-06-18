9d41cbe708be579c8daed8225fb84a6def934237
Akash Maity <am6030920@gmail.com>
2026-06-18 19:09:59 +0530

    kids product8

--- Frontend/src/pages/ProductDetails/Kids/KidProduct9.jsx
+++ Frontend/src/pages/ProductDetails/Kids/KidProduct9.jsx
@@ -1,4 +1,9 @@
-import React from 'react'
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import { CiHeart } from "react-icons/ci";
+import { FaHeart } from "react-icons/fa";
+import { IoIosGitCompare } from "react-icons/io";
+import { MdZoomOutMap } from "react-icons/md";
 
 const KidProduct9 = () => {
   return (
