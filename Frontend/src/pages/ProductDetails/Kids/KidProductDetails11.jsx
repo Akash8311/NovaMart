d60f531b34424baccf847bc923f546de59215cf6
Akash Maity <am6030920@gmail.com>
2026-06-22 10:26:46 +0530

    add kids product

---
+++ Frontend/src/pages/ProductDetails/Kids/KidProductDetails11.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const KidProductDetails11 = () => {
+  return (
+    <div>KidProductDetails11</div>
+  )
+}
+
+export default KidProductDetails11
---
+++ Frontend/src/pages/ProductDetails/Kids/KidProductDetails12.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const KidProductDetails12 = () => {
+  return (
+    <div>KidProductDetails12</div>
+  )
+}
+
+export default KidProductDetails12
