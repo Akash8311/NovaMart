6b8f77da21697bbc336d57ba867cf28213fc5f55
Akash Maity <am6030920@gmail.com>
2026-06-23 23:18:32 +0530

    edit  kids folder

--- Frontend/src/pages/ProductDetails/Kids/KidProductDetails9.jsx
+++ Frontend/src/pages/ProductDetails/Kids/KidProductDetails9.jsx
@@ -1,4 +1,34 @@
-import React from 'react'
+import React from "react";
+import Breadcrumbs from "@mui/material/Breadcrumbs";
+import Link from "@mui/material/Link";
+import HomeIcon from "@mui/icons-material/Home";
+import WhatshotIcon from "@mui/icons-material/Whatshot";
+import { Link as RouterLink } from "react-router-dom";
+import { IoShirt } from "react-icons/io5";
+import { CiTrophy } from "react-icons/ci";
+import { RiSecurePaymentLine } from "react-icons/ri";
+import { useState } from "react";
+import Qtybox from "../../../components/QtyBox/Qtybox";
+import "../Product.css";
+import { Swiper, SwiperSlide } from "swiper/react";
+
+import "swiper/css";
+import "swiper/css/pagination";
+
+import { Pagination } from "swiper/modules";
+import MenProduct1 from "../Men/MenProduct1";
+import MenProduct2 from "../Men/MenProduct2";
+import MenProduct3 from "../Men/MenProduct3";
+import MenProduct4 from "../Men/MenProduct4";
+import MenProduct5 from "../Men/MenProduct5";
+import MenProduct6 from "../Men/MenProduct6";
+import MenProduct7 from "../Men/MenProduct7";
+import MenProduct8 from "../Men/MenProduct8";
+import MenProduct9 from "../Men/MenProduct9";
+import ProductZoomMen1 from "../../../components/ProductZoom/ProductZoomMen1";
+import MenProductReview1 from "../ProductReaview/MenReview/MenProductReview1";
+
+
 
 const KidProductDetails9 = () => {
   return (
