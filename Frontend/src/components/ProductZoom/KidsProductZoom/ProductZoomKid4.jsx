34f595a075e5463c2b12d810b53eda1530a5e31b
Akash Maity <am6030920@gmail.com>
2026-06-25 19:16:40 +0530

    add kids folder

--- Frontend/src/components/ProductZoom/KidsProductZoom/ProductZoomKid4.jsx
+++ Frontend/src/components/ProductZoom/KidsProductZoom/ProductZoomKid4.jsx
@@ -1,5 +1,13 @@
-import React from 'react'
+import React, { useRef, useState } from "react";
+import "./ProductZoom.css";
 
+const images = [
+  "https://m.media-amazon.com/images/I/61MCKVRROeL._SY741_.jpg",
+  "https://m.media-amazon.com/images/I/61SOAIN5udL._SY741_.jpg",
+  "https://m.media-amazon.com/images/I/51L4C9ratuL._SY741_.jpg",
+  "https://m.media-amazon.com/images/I/81XyARMWd+L._SY741_.jpg",
+  "https://m.media-amazon.com/images/I/71rbIh+y5yL._SY741_.jpg",
+];
 const ProductZoomKid4 = () => {
   return (
     <div>ProductZoomKid4</div>
