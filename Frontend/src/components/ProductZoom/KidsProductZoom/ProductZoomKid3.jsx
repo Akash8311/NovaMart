9143cde7fcf945998718082d08efab22b70e72fb
Akash Maity <am6030920@gmail.com>
2026-06-25 19:16:02 +0530

    add kids folder

--- Frontend/src/components/ProductZoom/KidsProductZoom/ProductZoomKid3.jsx
+++ Frontend/src/components/ProductZoom/KidsProductZoom/ProductZoomKid3.jsx
@@ -1,4 +1,14 @@
-import React from 'react'
+import React, { useRef, useState } from "react";
+import "./ProductZoom.css";
+
+const images = [
+  "https://m.media-amazon.com/images/I/61MCKVRROeL._SY741_.jpg",
+  "https://m.media-amazon.com/images/I/61SOAIN5udL._SY741_.jpg",
+  "https://m.media-amazon.com/images/I/51L4C9ratuL._SY741_.jpg",
+  "https://m.media-amazon.com/images/I/81XyARMWd+L._SY741_.jpg",
+  "https://m.media-amazon.com/images/I/71rbIh+y5yL._SY741_.jpg",
+];
+
 
 const ProductZoomKid3 = () => {
   return (
