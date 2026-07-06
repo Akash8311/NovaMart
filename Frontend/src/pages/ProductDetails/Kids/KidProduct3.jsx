aa2d792a71cb559246e6a170e0032ffd439951d6
Akash Maity <am6030920@gmail.com>
2026-07-06 19:45:16 +0530

    kid 3

--- Frontend/src/pages/ProductDetails/Kids/KidProduct3.jsx
+++ Frontend/src/pages/ProductDetails/Kids/KidProduct3.jsx
@@ -156,7 +156,7 @@ const KidProduct3 = () => {
           </div>
   
           <Link
-            to="/MenProductDetails1"
+            to="/KidProductDetails3"
             style={{ textDecoration: "none", color: "#777" }}
           >
             <div
