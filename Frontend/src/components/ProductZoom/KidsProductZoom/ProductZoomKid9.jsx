cb538decee473916e06991a3e350f797bee3aa21
Akash Maity <am6030920@gmail.com>
2026-07-10 19:32:14 +0530

    kid 9 done

--- Frontend/src/components/ProductZoom/KidsProductZoom/ProductZoomKid9.jsx
+++ Frontend/src/components/ProductZoom/KidsProductZoom/ProductZoomKid9.jsx
@@ -2,11 +2,11 @@ import React, { useRef, useState } from "react";
 import "../ProductZoom.css";
 
 const images = [
-  "https://m.media-amazon.com/images/I/61MCKVRROeL._SY741_.jpg",
-  "https://m.media-amazon.com/images/I/61SOAIN5udL._SY741_.jpg",
-  "https://m.media-amazon.com/images/I/51L4C9ratuL._SY741_.jpg",
-  "https://m.media-amazon.com/images/I/81XyARMWd+L._SY741_.jpg",
-  "https://m.media-amazon.com/images/I/71rbIh+y5yL._SY741_.jpg",
+  "https://m.media-amazon.com/images/I/61P94ddh63L._SY741_.jpg",
+  "https://m.media-amazon.com/images/I/61CQrzd8MCL._SY741_.jpg",
+  "https://m.media-amazon.com/images/I/71NvaST43+L._SY741_.jpg",
+  "https://m.media-amazon.com/images/I/716JXhUKxXL._SY741_.jpg",
+  "https://m.media-amazon.com/images/I/61HWfOzzsGL._SY741_.jpg",
 ];
 const ProductZoomKid9 = () =>{
   const imgRef = useRef(null);
