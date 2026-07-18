163becb0b557f526e16a376e85e3030ef6433004
Akash Maity <am6030920@gmail.com>
2026-07-18 18:20:54 +0530

    update

--- Frontend/src/components/HomesSlider/HomeSlider.jsx
+++ Frontend/src/components/HomesSlider/HomeSlider.jsx
@@ -21,15 +21,14 @@ const HomeSlider = () => {
         className="sliderHome"
       >
         {[
-          "https://m.media-amazon.com/images/G/31/img2020/fashion/Topbrand-C/11.jpeg",
-          "https://images-eu.ssl-images-amazon.com/images/G/31/img24/Beauty/GW/Nov/UBS/updated/non_prime_22-1._CB780111954_.jpg",
-          "https://images-eu.ssl-images-amazon.com/images/G/31/img23/shrey/D328630419_PC_2_1x._CB797528247_.jpg",
-          "https://m.media-amazon.com/images/G/31/img20/Wireless/ElectronicsStore/GW/New/1242x450.jpg",
-          "https://serviceapi.spicezgold.com/download/1762393375689_34292.jpg",
-          "https://couponswala.com/blog/wp-content/uploads/2022/09/combo-offers-for-women.jpg.webp",
-          "https://images-eu.ssl-images-amazon.com/images/G/31/IMG24/Smart_Watches/META_RAYBANGLASS_NOV25/PC_Hero_3000X1200_Meta_1._CB779739080_.jpg",
-          "https://images-eu.ssl-images-amazon.com/images/G/31/img21/Raghu/GW_Hero/Nov2025/qwert/3000._CB779902781_.jpg",
-          "https://m.media-amazon.com/images/G/31/img21/Fashion/June_WRS/Event/Ingress/AF_set/Spotlight/Deals._SY700_QL85_.jpg",
+          "https://t3.ftcdn.net/jpg/06/08/19/10/360_F_608191088_ATXwUHQnOIe67Dnt7JDkzKWHDpgCfuCA.jpg",
+          "https://haniffa.com.sg/wp-content/uploads/2021/01/Main_Banner_KidsWear-1024x397.jpg",
+           "https://static.vecteezy.com/system/resources/thumbnails/028/714/271/small/fashion-week-male-model-isolated-on-gradient-background-with-a-place-for-text-photo.jpg",
+          "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSEKM4PacGJWYdsZFj_gIvS_Xv3a6PVJ7DEaWnd2Pstx7ch-1pvSq2Fqpt4&s=10",
+          "https://www.shutterstock.com/image-photo/banner-group-emotional-elementary-school-260nw-2411943543.jpg",
+          "https://img.magnific.com/free-vector/fashion-sale-landing-page-template_52683-40480.jpg?semt=ais_hybrid&w=740&q=80",
+          "https://t3.ftcdn.net/jpg/07/02/38/18/360_F_702381809_k0Jj93x9uwdtgqjja3mWDfWWj9ryHj7N.jpg",
+          "https://cdn.dribbble.com/userupload/35878077/file/original-9319da951c6e21470ad5b9e534db0245.jpg?resize=752x&vertical=center",
         ].map((src, i) => (
           <SwiperSlide key={i}>
             <div className="slide-container">
