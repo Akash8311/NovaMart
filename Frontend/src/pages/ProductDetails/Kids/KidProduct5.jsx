9dcee6eca7461bfb3435c236ac0c17a6e17dd78d
Akash Maity <am6030920@gmail.com>
2026-08-03 22:48:10 +0530

    update

--- Frontend/src/pages/ProductDetails/Kids/KidProduct5.jsx
+++ Frontend/src/pages/ProductDetails/Kids/KidProduct5.jsx
@@ -8,6 +8,7 @@ import { MdZoomOutMap } from "react-icons/md";
 
 const KidProduct5 = () => {
   const [wishlist, setWishlist] = useState(false);
+  const { wishlistItems, setWishlistItems } = useContext(MyContext);
   const [hover, setHover] = useState(false);
   const [isHovered, setIsHovered] = useState(false);
   return (
