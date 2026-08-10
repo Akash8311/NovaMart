dc399e050ad149481fb4071c3d07180abed55750
Akash Maity <am6030920@gmail.com>
2026-08-10 09:38:55 +0530

    update

--- Frontend/src/pages/ProductDetails/Men/MenProduct3.jsx
+++ Frontend/src/pages/ProductDetails/Men/MenProduct3.jsx
@@ -55,7 +55,17 @@ const MenProduct3 = () => {
           {/* Wishlist Button */}
           {hover && (
             <button
-              onClick={() => setWishlist(!wishlist)}
+              onClick={() => {
+                if (!wishlist) {
+                  setWishlistItems([...wishlistItems, product]);
+                } else {
+                  setWishlistItems(
+                    wishlistItems.filter((item) => item.id !== product.id),
+                  );
+                }
+
+                setWishlist(!wishlist);
+              }}
               style={{
                 width: "42px",
                 height: "42px",
