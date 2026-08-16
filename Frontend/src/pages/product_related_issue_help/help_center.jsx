3725a8141fbdd887283cfc73047ddc8f6a1bf91e
Akash Maity <am6030920@gmail.com>
2026-08-16 14:03:20 +0530

    update

---
+++ Frontend/src/pages/product_related_issue_help/help_center.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const help_center = () => {
+  return (
+    <div>help_center</div>
+  )
+}
+
+export default help_center
---
+++ Frontend/src/pages/product_related_issue_help/order_tracking.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const order_tracking = () => {
+  return (
+    <div>order_tracking</div>
+  )
+}
+
+export default order_tracking
