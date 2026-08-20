0a79a51f4d2d25f9d56830cc7c86f7f0a1b0e34e
Akash Maity <am6030920@gmail.com>
2026-08-20 20:21:23 +0530

    update

--- Frontend/src/pages/product_related_issue_help/order_tracking.jsx
+++ Frontend/src/pages/product_related_issue_help/order_tracking.jsx
@@ -10,4 +10,12 @@ const order_tracking = () => {
 }
 
 export default order_tracking
+import React from 'react'
 
+const order_tracking = () => {
+  return (
+    <div>order_tracking</div>
+  )
+}
+
+export default order_tracking
