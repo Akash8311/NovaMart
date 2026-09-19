ae076fd5653def84f3fc0ca982edaefb672b84e6
Akash8311 <am6030920@gmail.com>
2026-09-19 23:58:32 +0530

    update

--- Frontend/src/components/Header/Account/profile.jsx
+++ Frontend/src/components/Header/Account/profile.jsx
@@ -350,7 +350,7 @@ const Profile = () => {
         localStorage.removeItem("user");
         navigate("/");
     };
-
+fahhhhhhhh
     const activeIndex = TABS.findIndex((t) => t.id === activeTab);
 
     const infoRow = (field, icon, label) => (
